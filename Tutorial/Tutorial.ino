592883202bb2c77881c5fec8afee4df877c9dad5
Chad Geidel <lemadchef@gmail.com>
2021-11-26 16:08:43 -0700

    Part 3
    Iterating, displaying in game loop.

--- Tutorial/Tutorial.ino
+++ Tutorial/Tutorial.ino
@@ -1,12 +1,18 @@
 #include <Arduboy2.h>
 Arduboy2 arduboy;
 
+int counter;
+
 void setup() {
   arduboy.begin();
   arduboy.clear();
-  arduboy.print("Hello");
-  arduboy.display();
+  counter = 0;
 }
 
 void loop() {
+  arduboy.clear();
+  arduboy.setCursor(10, 10);
+  counter = counter + 1;
+  arduboy.print(counter);
+  arduboy.display();
 }
